e9cd66c90490618b5e989d5eb15832d9402cf16f
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2022-08-07 01:35:28 -0400

    Inicio con shared preferences básico

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,29 @@
 import 'package:flutter/material.dart';
-
 import 'src/pages/home_page.dart';
 import 'src/pages/login_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final prefs = await SharedPreferences.getInstance();
+  late final initialRoute = prefs.getString('token') ?? 'home';
+
+  return runApp(MyApp(initialRoute: initialRoute));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
+  final String initialRoute;
+  const MyApp({Key? key, required this.initialRoute}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
       debugShowCheckedModeBanner: false,
-      initialRoute: LoginPage.routeName,
+      initialRoute: initialRoute,
       routes: {
-        LoginPage.routeName : ( BuildContext context ) => const LoginPage(),
-        HomePage.routeName : ( BuildContext context ) => const HomePage(),
+        LoginPage.routeName: (BuildContext context) => const LoginPage(),
+        HomePage.routeName: (BuildContext context) => const HomePage(),
       },
     );
   }
